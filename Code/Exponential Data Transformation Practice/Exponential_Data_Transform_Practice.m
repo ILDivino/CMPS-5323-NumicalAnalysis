a9063326a83ad597c2af89e449ccdd88730cb87a
IlDivino <dragonmasterdrk@yahoo.com>
2023-07-13 17:35:59 -0500

    Exponential Transform complete

--- Code/Exponential Data Transformation Practice/Exponential_Data_Transform_Practice.m
+++ Code/Exponential Data Transformation Practice/Exponential_Data_Transform_Practice.m
@@ -2,18 +2,22 @@
 #Exponential Data Transformation Regression file
 
 #Converting a y=Ae^B into linear data so we can use linear regression.
+#             y_prime = B_prime + M_Prime * x
 
-#NOT COMPLETE
-#To finish in the evening!!!
+# B_prime is LN(A) remember that when converting.
+# M_prime = B
 
 # x = x
-# y = ln(y) in all instances except the raw data, when we think back to our formulas
+# y = ln(y) in regard to formula as it becomes y_prime
 
 
 
 #Our Data
 x = [2,5,7,10,14,19,26,31,34,38,45,52,53,60,65]
+#y is for raw data and graphing but is not used in any formula.
 y = [54,50,45,37,35,25,20,16,18,13,8,11,8,4,6]
+#y_prime the y used in all formula. y_prime = ln(y)
+y_prime = log(y)
 
 #Important that x and y have the same length, do not make me have to implement error detection!!!
 disp("\nData!!!\n")
@@ -22,31 +26,35 @@ N = length(x)
 disp("\nSums:\n")
 Sum_X = sum(x)
 Sum_X2 = sum(x.^2)
-Sum_LnY = sum(log(y))
-Sum_LnY2 = sum(log(y).^2)
-Sum_XLnY = sum(x.*log(y))
+Sum_Y = sum(y_prime)
+Sum_Y2 = sum(y_prime.^2)
+Sum_XY = sum(x.*y_prime)
 #Means
 disp("\nMeans!!\n")
-Mean_X = Sum_X/N
-Mean_LnY = Sum_LnY/N
-#this uses the S sub AB functions. Remember replace all instaces of y with ln(y)
-disp("\nS functions!!!\n")
-Sxy = Sum_XLnY - N*Mean_X*Mean_LnY
-Sxx = Sum_X2 - N*power(Mean_X,2)
-Syy = Sum_LnY2 - N*power(Mean_LnY,2)
+Mean_X = mean(x)
+Mean_Y = mean(y_prime)
+#this uses the L sub AB functions.
+disp("\nL functions!!!\n")
+Lxy = N*Sum_XY - Sum_X*Sum_Y
+Lxx = N*Sum_X2-power(Sum_X,2)
+Lyy = N*Sum_Y2-power(Sum_Y,2)
 #now for B, A, and the equation
 disp("\nB,A, and Equation!!!!\n")
-B = Sxy/Sxx
-A = exp(Mean_LnY - B*Mean_X)
-printf("Our equation is: y = %dE^%dx",A, B)
+B = Sxy/Sxx #also M_prime for linear
+B_prime = Mean_Y - (B * Mean_X)
+A = exp(B_prime)
+printf("Our linear question is: y = (%d)x+%d",B, B_prime)
+printf("Our exponential equation is: y = %dE^%dx",A, B)
 #now for R value
 disp("\nR-Value:\n")
 R = Sxy / (sqrt(Sxx)*sqrt(Syy))
 #Now to add plotting this out.
 #Even for testing I don't care to output this so it will have the ;
+#y2 = B * x + B_prime #linear representation
 y2 = A.*exp(B.*x);
 #we are plotting the initial x,y data as red circles 'or'
 #Our new equation will be plotted as a regular line x,y2
+#plot(x,y,'or',x,exp(y2)) #linear representation
 plot(x,y,'or',x,y2)
 xlabel("X Units")
 ylabel("Y Units")
