fd1b9d6887d7d2fb68a39d25d98878fa27b9d104
IlDivino <dragonmasterdrk@yahoo.com>
2023-08-02 20:25:27 -0500

    update

---
+++ Code/Euler's Method/Euler_Method.m
@@ -0,0 +1,50 @@
+#Michael Ellerkamp, NumAnalysis
+#Euler's Method file
+#Differential Equations
+#Getting into Octave functions
+a = 0;
+b = 0;
+function result = Euler_Method (a, b)
+    result = 3* exp(-a) - 0.4 * b;
+endfunction
+#used to solve partial differential equations
+#dy/dx + 0.4y = 3e^{-x} ; given value: y(0) = 5
+#dy/dx = 3e^{-x} - 0.4y ;; target:find y(x) =
+#H = step size.
+#I like the interval method so take target x - given x divide by number of
+#intervals to get H.
+
+display("y_{i+1} = y_i + f(x_i , y_i) * H\n")
+
+N = 5
+
+#equation, given, and target.
+#These matrices are where I will store all my x and y values for recall.
+X = [];
+Y = [];
+
+display("Given Values!\n")
+x = 0;
+y = 5;
+X = [X x]
+Y = [Y y]
+
+x = 3;
+printf("Target Value! f(%d) = ?\n", x)
+
+H = (x-X(1)) / N
+Iter = 0;
+MaxIter = N;
+
+do
+  Iter += 1;
+  printf("Iteration #%d!\n", Iter)
+  result = Euler_Method(X(Iter),Y(Iter))
+  x = H * Iter
+  y = Y(Iter) + result * H
+  Y = [Y y];
+  X = [X x];
+
+until (Iter >= MaxIter)
+X
+Y
---
+++ Code/Runge Kutta 4th Order/Runge_Kutta_4th_Order_Practice.m
@@ -0,0 +1,56 @@
+#Michael Ellerkamp, NumAnalysis
+#Runge Kutta 4th Order Method file
+#Differential Equations
+#Getting into Octave functions
+a = 0;
+b = 0;
+function result = Runge_Kutta_4th_Order_Practice (a, b)
+    result = 3* exp(-a) - 0.4 * b;
+endfunction
+#used to solve partial differential equations
+#dy/dx + 0.4y = 3e^{-x} ; given value: y(0) = 5
+#dy/dx = 3e^{-x} - 0.4y ;; target:find y(x) =
+#H = step size.
+#I like the interval method so take target x - given x divide by number of
+#intervals to get H.
+
+display("y_{i+1} = y_i + f(x_i , y_i) * H\n")
+
+N = 2
+
+#equation, given, and target.
+#These matrices are where I will store all my x and y values for recall.
+X = [];
+Y = [];
+
+display("Given Values!\n")
+x = 0;
+y = 5;
+X = [X x]
+Y = [Y y]
+
+x = 3;
+printf("Target Value! f(%d) = ?\n", x)
+
+H = (x-X(1)) / N
+Iter = 0;
+MaxIter = N;
+
+do
+  Iter += 1;
+  printf("Iteration #%d!\n", Iter)
+  result = Euler_Method(X(Iter),Y(Iter));
+  K_1 = result
+  result = Euler_Method(X(Iter)+.5*H,Y(Iter)+.5*H*K_1);
+  K_2 = result
+  result = Euler_Method(X(Iter)+.5*H,Y(Iter)+.5*H*K_2);
+  K_3 = result
+  result = Euler_Method(X(Iter)+H,Y(Iter)+K_3*H);
+  K_4 = result
+  x = H * Iter
+  y = Y(Iter) + (1/6)*(K_1 + 2*K_2 + 2*K_3 + K_4) * H
+  Y = [Y y];
+  X = [X x];
+until (Iter >= MaxIter)
+X
+Y
